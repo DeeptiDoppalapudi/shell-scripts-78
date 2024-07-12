d91adee9dc6a24bd15b6edd50e073951c3b519bb
Your Name <you@example.com>
2024-07-12 22:35:48 +0530

    Hello Script

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ P1=Sachin
 P2=Rahul
 
 echo "$P1:: Hello $P2, How are you?"
-ehco "$P2:: Hi $P1, I am fine Hru"
-ehco "$P1:: I am fine too, how is your work:"
-ehco "$P2:: Not bad"
+echo "$P2:: Hi $P1, I am fine Hru"
+echo "$P1:: I am fine too, how is your work:"
+echo "$P2:: Not bad"
